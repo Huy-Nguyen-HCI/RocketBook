13b8fe5236f453d94efa9c6931315d6f8eec7b99
vuh <vuh@93b6c1a2-f1de-42f6-8165-1409d5bbc26f>
2016-04-05 22:06:30 +0000

    Documented accountdb.h
    git-svn-id: https://svn.riouxsvn.com/rocketbook@32 93b6c1a2-f1de-42f6-8165-1409d5bbc26f

--- database/accountdb.h
+++ database/accountdb.h
@@ -7,6 +7,16 @@
 #include <QtSql/QSqlRecord>
 #include <QDebug>
 
+/**
+ * @brief The AccountDB class
+ *
+ * Create an sqlite3 table in a database file.
+ * The database structure:
+ * Column 1: accountid INTEGER PRIMARY KEY
+ * Column 2: username TEXT
+ * Column 3: password TEXT
+ * Column 4: profileid INTEGER
+ */
 class AccountDB
 {
 public:
@@ -41,22 +51,41 @@ public:
 
     /**
      * @brief addAccount
-     * Add a new account
+     * Add a new account to the database table
+     *
      * @param accountID Account ID
      * @param userName
      * @param password
      * @param profileid
-     * @return
+     * @return true if added, false if not added
      */
     bool addAccount(int accountID, const QString &userName, const QString &password, int profileid);
 
+    /**
+     * @brief removeAccount
+     * Remove an entire account from the database, knowing the username
+     *
+     * @param userName the username of the account removed
+     * @return true if succeeded, false if failed
+     */
     bool removeAccount(const QString& userName);
 
+    /**
+     * @brief accountExists
+     * Check if account exists, knowing the username
+     *
+     * @param userName the username of the account checked
+     * @return true if yes, false if no
+     */
     bool accountExists(const QString& userName) const;
 
+    /**
+     * @brief removeAllAccounts
+     * Remove all accounts existed in the database
+     * @return true if succeeded, false if failed
+     */
     bool removeAllAccounts();
 
-    bool removeAllProfiles();
 
 private:
     QSqlDatabase accountDB;
