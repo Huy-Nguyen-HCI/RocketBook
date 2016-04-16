053806fa9deda2c2ec348e845baeaccf1f0b9320
nguyenha <nguyenha@93b6c1a2-f1de-42f6-8165-1409d5bbc26f>
2016-04-16 20:21:07 +0000

    Add image sources to comment section in main.cpp of GUI project
    git-svn-id: https://svn.riouxsvn.com/rocketbook@142 93b6c1a2-f1de-42f6-8165-1409d5bbc26f

--- gui/main.cpp
+++ gui/main.cpp
@@ -4,8 +4,17 @@
 
 /**
     IMAGE SOURCES:
-    rocket.png: https://thenounproject.com/term/rocket/1758/
-
+    rocket.png:         https://thenounproject.com/term/rocket/1758/
+    message.png:        http://icons.iconarchive.com/icons/osullivanluke/orb-os-x/512/Messages-icon.png
+    post.png:           https://cdn1.iconfinder.com/data/icons/social-media-3/512/615556-Pencil_Document-512.png
+    group.png:          http://icons.iconarchive.com/icons/hopstarter/sleek-xp-basic/256/User-Group-icon.png
+    logout.png:         https://cdn1.iconfinder.com/data/icons/web-ui/30/power-01-128.png
+    feed.png:           https://icons8.com/
+    scrapbook.png:      https://icons8.com/
+    friends.png:        https://icons8.com/
+    notification.png:   https://icons8.com/
+    user.png:           https://icons8.com/
+    settings.png:       https://icons8.com/
   */
 
 
