b16d57dd000782be2ef1c624deb1fa73eda73178
townleym <townleym@93b6c1a2-f1de-42f6-8165-1409d5bbc26f>
2016-04-28 18:47:01 +0000

    scrolling on feed
    git-svn-id: https://svn.riouxsvn.com/rocketbook@317 93b6c1a2-f1de-42f6-8165-1409d5bbc26f

--- textui/feedui.cpp
+++ textui/feedui.cpp
@@ -4,7 +4,14 @@ FeedUI::FeedUI(AccountController* accountControl)
 {
     this->accountControl=accountControl;
     initialize();
-    displayFeed();
+    int chosenBlog=displayFeed();
+    if(chosenBlog!=-1)
+        viewBlog(chosenBlog);
+
+
+
+    //run();
+    //displayFeed();
     //options=2;
     //takeCommand(select(options));
     //run();
@@ -13,9 +20,12 @@ FeedUI::FeedUI(AccountController* accountControl)
 
 void FeedUI::run()
 {
-
-
+//    int chosenFriend=selectFriend();
+//    if(chosenFriend!=-1)
+//        viewFriendsProfile(chosenFriend);
 }
+
+
 
 void FeedUI::drawScreen(int v) {
 
@@ -46,74 +56,241 @@ void FeedUI::takeCommand(int selection){
 
 }
 
-void FeedUI::displayFeed()
+int FeedUI::displayFeed()
 {
-    bool booking=true;
+//    bool booking=true;
+//    int offset=0;
+//    int row;
+//    while(booking){
+
+
+//    row = 2;
+
+
+//    Feed *allFeed = accountControl->getUser()->getFeed();
+//    allFeed->updatePostList();
+//    std::vector<Post*> friendFeed = allFeed->getFeed();
+//    erase();
+//    refresh();
+//    mvprintw(0,0, "Your friends' rocket adventures.  Select a blog to comment");
+//    mvprintw(row, 5, "->");
+//    refresh();
+//    //int row=2;
+//    int *max;
+//    if(friendFeed.size() < 5){
+//        *max = friendFeed.size();
+//    }else{
+//        *max = offset+5;
+//    }
+//    for (unsigned int i = offset; i < *max; i++) {
+//        Post* currentPost = friendFeed[i];
+//        QString *author = new QString(currentPost->getAuthorUsername());
+//        Post::PostType pType = currentPost->type();
+//        switch (pType) {
+//            case Post::typeBlog:
+//                displayBlog((Blog*)currentPost, row, author);
+//                refresh();
+//                row = row+5;
+//                break;
+//            case Post::typeTweet:
+//                displayTweet((Tweet*)currentPost, row, author);
+//                refresh();
+//                row = row+5;
+//                break;
+//            case Post::typeMultimedia:
+//                mvprintw(row, 8, "multimedia content!");
+//                refresh();
+//                row = row+5;
+//                break;
+//            case Post::typeComment:
+//                break;
+//            case Post::typePost:
+//                break;
+//        }
+//    }
+
+//    //getch();
+//    int ch= getch();
+//        if(ch==KEY_DOWN && /*(*max!=(offset+5)))*/  !((friendFeed.size())==(5+offset)))//ch==258 || KEY_DOWN || !((wholeScrapbook.size()-5)>offset)) //259 and 259 enables scrolling
+//            offset++;
+//        else if(ch==KEY_UP && offset>0)//ch==259 || KEY_UP || offset>0)
+//            offset--;
+//        else if(ch==KEY_UP);
+//        else if(ch==KEY_DOWN);
+//        else if(ch==10)//enter key
+//            viewBlog(offset);
+
+
+//        else
+//            return -1;
+
+
+//    }
+//    return offset;
+
+    bool scrolling=true;
     int offset=0;
-    int row;
-    while(booking){
+    int row, max;
 
-
-    row = 2;
-
+    while(scrolling){
     Feed *allFeed = accountControl->getUser()->getFeed();
     allFeed->updatePostList();
     std::vector<Post*> friendFeed = allFeed->getFeed();
     erase();
     refresh();
-    mvprintw(0,0, "Your friends' rocket adventures:");
+    mvprintw(0,0, "Your Feed.  Select a blog");
     refresh();
-    //int row=2;
-    int *max;
-    if(friendFeed.size() < 5){
-        *max = friendFeed.size();
-    }else{
-        *max = offset+5;
-    }
-    for (unsigned int i = offset; i < *max; i++) {
-        Post* currentPost = friendFeed[i];
-        QString *author = new QString(currentPost->getAuthorUsername());
-        //QString *author = new QString("test author");
-        Post::PostType pType = currentPost->type();
-        switch (pType) {
-            case Post::typeBlog:
-                displayBlog((Blog*)currentPost, row, author);
-                refresh();
-                row = row+5;
-                break;
-            case Post::typeTweet:
-                displayTweet((Tweet*)currentPost, row, author);
-                refresh();
-                row = row+4;
-                break;
-            case Post::typeMultimedia:
-                mvprintw(row, 1, "multimedia content!");
-                refresh();
-                row = row+2;
-                break;
-            case Post::typeComment:
-                break;
-            case Post::typePost:
-                break;
-        }
-    }
 
-    //getch();
+    row=3;
+    mvprintw(row, 5, "->");
+    refresh();
+
+
+
+    max=offset+5;
+    if(friendFeed.size()<offset+5)
+        max=friendFeed.size();
+
+    for(unsigned int i=offset;i<max; i++){
+        //mvprintw(row,8,std::to_string(chatList->at(i)).c_str());
+        //row++;
+        Post* currentPost = friendFeed[i];
+               QString *author = new QString(currentPost->getAuthorUsername());
+               Post::PostType pType = currentPost->type();
+               switch (pType) {
+                   case Post::typeBlog:
+                       displayBlog((Blog*)currentPost, row, author);
+                       refresh();
+                       row = row+5;
+                       break;
+                   case Post::typeTweet:
+                       displayTweet((Tweet*)currentPost, row, author);
+                       refresh();
+                       row = row+5;
+                       break;
+                   case Post::typeMultimedia:
+                       mvprintw(row, 8, "multimedia content!");
+                       refresh();
+                       row = row+5;
+                       break;
+                   case Post::typeComment:
+                       break;
+                   case Post::typePost:
+                       break;
+               }
+           }
+
+
     int ch= getch();
-        if(ch==KEY_DOWN && !((friendFeed.size())==(5+offset)))//ch==258 || KEY_DOWN || !((wholeScrapbook.size()-5)>offset)) //259 and 259 enables scrolling
+        if(ch==KEY_DOWN && (max!=(offset+1)))//ch==258 || KEY_DOWN || !((wholeScrapbook.size()-5)>offset)) //259 and 259 enables scrolling
             offset++;
         else if(ch==KEY_UP && offset>0)//ch==259 || KEY_UP || offset>0)
             offset--;
         else if(ch==KEY_UP);
         else if(ch==KEY_DOWN);
-
-
+        else if(ch==10)//enter key
+            viewBlog(offset);
         else
-            booking=false;
-
+            return -1;
 
     }
+    return offset;
+
+
+
+
+
+
 }
+
+void FeedUI::viewBlog(int index)
+{
+    bool isBlog = checkType(index);
+    if( !isBlog){
+        return;
+    }
+    Feed *allFeed = accountControl->getUser()->getFeed();
+    allFeed->updatePostList();
+    std::vector<Post*> friendFeed = allFeed->getFeed();
+    erase();
+    refresh();
+    Post* currentPost = friendFeed[index];
+    QString *author = new QString(currentPost->getAuthorUsername());
+
+
+
+}
+
+bool FeedUI::checkType(int postIndex)
+{
+    Feed *allFeed = accountControl->getUser()->getFeed();
+    allFeed->updatePostList();
+    std::vector<Post*> friendFeed = allFeed->getFeed();
+    erase();
+    refresh();
+    Post* currentPost = friendFeed[postIndex];
+    Post::PostType pType = currentPost->type();
+    switch (pType) {
+        case Post::typeBlog:
+           return true;
+            break;
+        case Post::typeTweet:
+            return false;
+            break;
+        case Post::typeMultimedia:
+            return false;
+            break;
+        case Post::typeComment:
+             return false;
+            break;
+        case Post::typePost:
+        return false;
+            break;
+    }
+
+}
+
+
+//int FeedUI::selectBlog(){
+
+//    bool scrolling=true;
+//    int offset=0;
+//    int row, max;
+
+//    while(scrolling){
+//    erase();
+//    mvprintw(0,0, "Friends: Press enter to select friend");
+
+//    row=3;
+//    mvprintw(row, 5, "->");
+
+//    QStringList friendNames= friendControl->getFriendNames();
+
+//    max=offset+15;
+//    if(friendNames.size()<offset+15)
+//        max=friendNames.size();
+
+//    for (unsigned int i = offset; i < max; i++) {
+//        mvprintw(row,8,friendNames.at(i).toStdString().c_str());
+//        row++;
+//    }
+
+//    int ch= getch();
+//        if(ch==KEY_DOWN && (max!=(offset+1)))//ch==258 || KEY_DOWN || !((wholeScrapbook.size()-5)>offset)) //259 and 259 enables scrolling
+//            offset++;
+//        else if(ch==KEY_UP && offset>0)//ch==259 || KEY_UP || offset>0)
+//            offset--;
+//        else if(ch==KEY_UP);
+//        else if(ch==KEY_DOWN);
+//        else if(ch==10)//enter key
+//            scrolling=false;
+//        else
+//            return -1;
+
+//    }
+//    return offset;
+
+//}
 
 void FeedUI::displayBlog(Blog *blog, int row, QString *author)
 {
@@ -122,9 +299,9 @@ void FeedUI::displayBlog(Blog *blog, int row, QString *author)
     QString currentContent = blog->getContent();
     QString line1;
     line1 = *author + "'s blog:";
-    mvprintw(row, 1, line1.toStdString().c_str());
-    mvprintw(row+1, 1, currentTitle.toStdString().c_str());
-    mvprintw(row+2, 1, currentContent.toStdString().c_str());
+    mvprintw(row,8, line1.toStdString().c_str());
+    mvprintw(row+1, 8, currentTitle.toStdString().c_str());
+    mvprintw(row+2, 8, currentContent.toStdString().c_str());
     refresh();
 
 }
@@ -134,8 +311,8 @@ void FeedUI::displayTweet(Tweet *tweet, int row, QString *author)
     QString currentContent = tweet->getContent();
     QString line1;
     line1 = *author + "'s tweet:";
-    mvprintw(row,1, line1.toStdString().c_str());
-    mvprintw(row+1, 1, currentContent.toStdString().c_str());
+    mvprintw(row,8, line1.toStdString().c_str());
+    mvprintw(row+1, 8, currentContent.toStdString().c_str());
     refresh();
 
 }
--- textui/feedui.h
+++ textui/feedui.h
@@ -10,7 +10,10 @@ public:
     void drawScreen(int v);
     void takeCommand(int selection);
     void run();
-    void displayFeed();
+    int displayFeed();
+    int selectBlog();
+    void viewBlog(int index);
+    bool checkType(int postIndex);
     void displayBlog(Blog* blog, int row, QString* author);
     void displayTweet(Tweet* tweet, int row, QString* author);
 };
