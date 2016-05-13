9c559f8bb841be787f1962fe97e2c0e113fc81f4
townleym <townleym@93b6c1a2-f1de-42f6-8165-1409d5bbc26f>
2016-05-13 20:39:14 +0000

    finishing touches
    git-svn-id: https://svn.riouxsvn.com/rocketbook@366 93b6c1a2-f1de-42f6-8165-1409d5bbc26f

--- unittest/unittest/modeltester.cpp
+++ unittest/unittest/modeltester.cpp
@@ -73,6 +73,29 @@ TEST(RocketUserTest, testPhoto)
 
 }
 
+TEST(RocketUserTest, testGetProfile)
+{
+
+    const QString dbPath("../unittest/testdirec");
+    int id1 = 0;
+    const QString un1("username1");
+    const QString fn1("fullName1");
+    const QString ph1("photo1");
+    QString dscr1("description1");
+    int id2 = 1;
+    const QString un2("username2");
+    const QString fn2("fullName2");
+    const QString ph2("photo2");
+    QString dscr2("description2");
+
+    RocketUser user1(dbPath, id1, un1, fn1, ph1, dscr1);
+    ASSERT_EQ(ph1, user1.getProfile()->getPicturePath());
+    ASSERT_EQ(dscr1, user1.getProfile()->getDescription());
+    ASSERT_EQ(fn1, user1.getProfile()->getFullName());
+
+
+}
+
 //Profile Tests:
 
 TEST(ProfileTest, testGetProfileID)
@@ -149,4 +172,159 @@ TEST(ProfileTest, testPicturePath)
     ASSERT_NE(ph1, prof.getPicturePath());
 
 }
+
+//Blog tests
+
+TEST(BlogTest, testTitle)
+{
+    QString username1("username 1");
+    QString title1("title 1");
+    QString content1("content 1");
+    QString username2("username 2");
+    QString title2("title 2");
+    QString content2("content 2");
+    QString dbPath("../unittest/testdirec");
+
+    Blog newBlog1(username1, title1, content1, dbPath);
+    Blog newBlog2(username2, title2, content2, dbPath);
+
+    ASSERT_EQ(title1, newBlog1.getTitle());
+    ASSERT_EQ(title2, newBlog2.getTitle());
+
+}
+
+TEST(BlogTest, testAuthor)
+{
+    QString username1("username 1");
+    QString title1("title 1");
+    QString content1("content 1");
+    QString username2("username 2");
+    QString title2("title 2");
+    QString content2("content 2");
+    QString dbPath("../unittest/testdirec");
+
+    Blog newBlog1(username1, title1, content1, dbPath);
+    Blog newBlog2(username2, title2, content2, dbPath);
+
+    ASSERT_EQ(username1, newBlog1.getAuthorUsername());
+    ASSERT_EQ(username2, newBlog2.getAuthorUsername());
+
+}
+
+TEST(BlogTest, testEditContent)
+{
+    QString username1("username 1");
+    QString title1("title 1");
+    QString content1("content 1");
+    QString username2("username 2");
+    QString title2("title 2");
+    QString content2("content 2");
+    QString dbPath("../unittest/testdirec");
+
+    Blog newBlog1(username1, title1, content1, dbPath);
+    Blog newBlog2(username2, title2, content2, dbPath);
+
+    ASSERT_EQ(content1, newBlog1.getContent());
+    ASSERT_EQ(content2, newBlog2.getContent());
+    newBlog1.editBlog(content2);
+    newBlog2.editBlog(content1);
+    ASSERT_EQ(content2, newBlog1.getContent());
+    ASSERT_EQ(content1, newBlog2.getContent());
+
+}
+
+TEST(BlogTest, testComments)
+{
+    QString username1("username 1");
+    QString title1("title 1");
+    QString content1("content 1");
+    QString username2("username 2");
+    QString title2("title 2");
+    QString content2("content 2");
+    QString dbPath("../unittest/testdirec");
+    QString comment1("comment 1");
+    QString comment2("comment 2");
+
+    Blog newBlog1(username1, title1, content1, dbPath);
+    Blog newBlog2(username2, title2, content2, dbPath);
+
+    newBlog1.addComment(username1, comment1);
+    newBlog1.addComment(username2, comment2);
+    newBlog2.addComment(username2, comment2);
+    newBlog2.addComment(username1, comment1);
+
+    std::vector<Comment*> comments1 = newBlog1.getAllComments();
+    std::vector<Comment*> comments2 = newBlog2.getAllComments();
+
+    ASSERT_EQ(2, comments1.size());
+    ASSERT_EQ(2, comments2.size());
+
+    ASSERT_EQ(comment1, comments1.at(0)->getContent());
+    ASSERT_EQ(comment2, comments1.at(1)->getContent());
+    ASSERT_EQ(username1, comments1.at(0)->getAuthorUsername());
+    ASSERT_EQ(username2, comments1.at(1)->getAuthorUsername());
+    ASSERT_EQ(comment1, comments2.at(1)->getContent());
+    ASSERT_EQ(comment2, comments2.at(0)->getContent());
+    ASSERT_EQ(username1, comments2.at(1)->getAuthorUsername());
+    ASSERT_EQ(username2, comments2.at(0)->getAuthorUsername());
+}
+
+//Comment tests
+
+TEST(CommentTests, testContent)
+{
+    QString username1("username 1");
+    QString content1("content 1");
+    QString username2("username 2");
+    QString content2("content 2");
+
+    Comment newComment1(username1, content1);
+    Comment newComment2(username2, content2);
+
+    ASSERT_EQ(content1, newComment1.getContent());
+    ASSERT_EQ(content2, newComment2.getContent());
+}
+
+TEST(CommentTests, testAuthor)
+{
+    QString username1("username 1");
+    QString content1("content 1");
+    QString username2("username 2");
+    QString content2("content 2");
+
+    Comment newComment1(username1, content1);
+    Comment newComment2(username2, content2);
+
+    ASSERT_EQ(username1, newComment1.getAuthorUsername());
+    ASSERT_EQ(username2, newComment2.getAuthorUsername());
+}
+
+//Group Test
+
+TEST(GroupTests, testGetID)
+{
+    const QString dbPath("../unittest/testdirec");
+    int gid = 1;
+    int aid = 3;
+    QString name("Full Name");
+    QString photo("Photo");
+    QString dscr("Description");
+    Group newGroup(dbPath, gid, aid, name, photo, dscr);
+    ASSERT_EQ(gid, newGroup.getID());
+}
+
+TEST(GroupTests, testProfile)
+{
+    const QString dbPath("../unittest/testdirec");
+    int gid = 1;
+    int aid = 3;
+    QString name("Full Name");
+    QString photo("Photo");
+    QString dscr("Description");
+    Group newGroup(dbPath, gid, aid, name, photo, dscr);
+    ASSERT_EQ(name, newGroup.getProfile()->getFullName());
+    ASSERT_EQ(photo, newGroup.getProfile()->getPicturePath());
+    ASSERT_EQ(dscr, newGroup.getProfile()->getDescription());
+}
+
 
